162868db796184c1e1d4350b0ad1596bf77a0b9c
Vincent <thisoneis4business@gmail.com>
2021-11-02 23:36:30 +0700

    Fix: Removed useless code comment

--- Sources/Desolate/Decorator/Desolated.swift
+++ Sources/Desolate/Decorator/Desolated.swift
@@ -14,31 +14,6 @@ struct Desolated<Value> {
     private var innerHook: Desolate<AsyncCapsule<Value>>
     private var timeout: TimeInterval
     private var fallback: () -> Value
-//
-//    static subscript<T>(
-//            _enclosingInstance instance: T,
-//            wrapped wrappedKeyPath: ReferenceWritableKeyPath<T, Value>,
-//            storage storageKeyPath: ReferenceWritableKeyPath<T, Self>
-//    ) -> Value {
-//        get {
-//            let hook: Desolate<StateHook<Value>> = instance[keyPath: storageKeyPath].innerHook
-//            let timeout: TimeInterval = instance[keyPath: storageKeyPath].timeout
-//            let fallback: () -> Value = instance[keyPath: storageKeyPath].fallback
-//            let res = conduit(timeout: timeout) {
-//                try await hook.ask(timeout: timeout) { .get(ref: $0)  }
-//            }
-//            switch res {
-//            case .success(let val):
-//                return val
-//            case .failure(_):
-//                return fallback()
-//            }
-//
-//        } set {
-//            let hook: Desolate<StateHook<Value>> = instance[keyPath: storageKeyPath].innerHook
-//            hook.tell(with: .set(new: newValue))
-//        }
-//    }
 
     var wrappedValue: Value {
         get {
